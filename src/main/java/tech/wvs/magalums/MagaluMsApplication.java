170a2a7c898ccf992a17256ad4dfc68df260fee4
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-13 15:35:23 -0300

    Create Spring app

---
+++ src/main/java/tech/wvs/magalums/MagaluMsApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.magalums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagaluMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/magalums/MagaluMsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
